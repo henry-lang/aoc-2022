ea944da8fee2c727497ebf295cb881dca3563c5d
henry-lang <henry.langmack@gmail.com>
2022-12-09 18:52:49 -0500

    day 7 cool solution, day 8 unfinished

--- src/day7.rs
+++ src/day7.rs
@@ -1,5 +1,3 @@
-use std::{collections::HashMap, rc::Rc, str::FromStr};
-
 enum Command<'a> {
     List,
     Cd(&'a str),
@@ -7,12 +5,12 @@ enum Command<'a> {
     FileInfo(&'a str, usize),
 }
 
-impl FromStr for Command<'_> {
-    type Err = ();
+impl<'a> TryFrom<&'a str> for Command<'a> {
+    type Error = ();
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+    fn try_from(s: &'a str) -> Result<Command<'a>, Self::Error> {
         let mut words = s.split_whitespace();
-        let next_word = || words.next().ok_or(());
+        let mut next_word = || words.next().ok_or(());
         let first = next_word()?;
         match first.chars().next().ok_or(())? {
             '0'..='9' => Ok(Self::FileInfo(
@@ -25,60 +23,91 @@ impl FromStr for Command<'_> {
                 "ls" => Ok(Self::List),
                 _ => Err(()),
             },
+            _ => panic!(),
         }
     }
 }
 
-enum Node<'a> {
-    Dir(Option<Rc<Node<'a>>>, HashMap<&'a str, Rc<Node<'a>>>),
-    File(&'a str, usize),
+struct Node {
+    size: usize, // Enables caching in immutable struct
+    children: Vec<Node>,
+}
+
+impl Node {
+    pub fn traverse_dirs(&self) -> Box<dyn Iterator<Item = &Node> + '_> {
+        Box::new(
+            self.children
+                .iter()
+                .filter(|c| !c.children.is_empty())
+                .flat_map(|c| c.traverse_dirs())
+                .chain(std::iter::once(self)),
+        )
+    }
+
+    pub fn add_child(&mut self, child: Node) {
+        self.size += child.size;
+        self.children.push(child);
+    }
+
+    pub fn build<'a>(mut self, commands: &mut impl Iterator<Item = Command<'a>>) -> Self {
+        while let Some(command) = commands.next() {
+            use Command::*;
+            match command {
+                Cd(path) => match path {
+                    ".." => {
+                        break; // We've exited out of this directory so we're done
+                    }
+                    _ => {
+                        self.add_child(Node {
+                            size: 0,
+                            children: Vec::new(),
+                        }.build(commands))
+                    }
+                },
+                FileInfo(_, size) => {
+                    self.add_child(Node {
+                        children: Vec::new(),
+                        size,
+                    });
+                }
+                _ => {}
+            }
+        }
+
+        self
+    }
 }
 
 fn build_fs(input: &str) -> Node {
-    let mut root = Rc::new(Node::Dir(None, Default::default()));
-    let mut current_node = root.clone();
-
     let mut commands = input
         .lines()
-        .map(|l| l.parse::<Command>().expect("parse command"));
+        .map(|l| Command::try_from(l).expect("parse command"))
+        .peekable();
 
-    while let Some(command) = commands.next() {
-        use Command::*;
-        match command {
-            Cd(path) => match path {
-                ".." => {
-                    current_node = match current_node.as_ref() {
-                        Node::Dir(parent, _) => parent.unwrap(),
-                        _ => panic!(),
-                    }
-                    .clone()
-                }
-                "/" => {}
-                _ => {
-                    current_node = match current_node.as_ref() {
-                        Node::Dir(_, children) => children,
-                        _ => panic!(),
-                    }
-                    .get(path)
-                    .unwrap()
-                    .clone();
-                }
-            },
-            List => {
-                for command in commands.take
-            }
-        }
+    Node {
+        size: 0,
+        children: Vec::new(),
     }
-
-    root
+    .build(&mut commands)
 }
 
 pub fn part_a(input: &str) -> impl ToString {
-    let fs = build_fs(input);
+    build_fs(input)
+        .traverse_dirs()
+        .map(|d| d.size)
+        .filter(|&size| size <= 100_000)
+        .sum::<usize>()
 }
 
 pub fn part_b(input: &str) -> impl ToString {
-    0
+    let fs = build_fs(input);
+    let needed = 30_000_000 - (70_000_000 - fs.size);
+
+    fs.traverse_dirs()
+        .map(|d| d.size)
+        .filter(|&size| size >= needed)
+        .min()
+        .unwrap_or(0)
 }
 
-crate::test_day!(7, 95437, 0);
+crate::test_day!(7, 95437, 24933642);
---
+++ src/day8.rs
@@ -0,0 +1,12 @@
+// const DIRS: [(i8, i8); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+// pub fn part_a(input: &str) -> impl ToString {
+//     let map = input.lines().map(|l| l.chars().map(|c| c as u8 - b'0').collect()).collect::<Vec<Vec<_>>>();
+//     map.iter().enumerate().map(|(i, r)| row.iter().enumerate().map(|(j, c)| ))).sum()
+// }
+
+// pub fn part_b(input: &str) -> impl ToString {
+//     0
+// }
+
+// crate::test_day!(8, 21, 0);
